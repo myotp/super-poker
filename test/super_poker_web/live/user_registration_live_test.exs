e3415b732152533283e80fff1a6e4c13ebe297e7
jia.wang <wj2016@gmail.com>
2023-10-11 15:22:29 +0200

    [2023-10-11][LiveView] 简单修复或者跳过LiveView测试

--- test/super_poker_web/live/user_registration_live_test.exs
+++ test/super_poker_web/live/user_registration_live_test.exs
@@ -48,7 +48,7 @@ defmodule SuperPokerWeb.UserRegistrationLiveTest do
       assert redirected_to(conn) == ~p"/"
 
       # Now do a logged in request and assert on the menu
-      conn = get(conn, "/")
+      conn = get(conn, "/lobby")
       response = html_response(conn, 200)
       assert response =~ email
       assert response =~ "Settings"
