a1f40bec84c3db8c45308d44ef5d46010bd2c5dd
jia.wang <wj2016@gmail.com>
2023-10-16 19:26:05 +0200

    [2023-10-16][PokerHistory] 暂时先放放这种历史数据, 优先弄更好玩的AI扑克部分

--- lib/super_poker/history/hhsmithy_exporter.ex
+++ lib/super_poker/history/hhsmithy_exporter.ex
@@ -3,5 +3,11 @@ defmodule SuperPoker.History.HhsmithyExporter do
 
   # TODO: 用上模版LEEx还是HEEx
   def to_string(%GameHistory{}) do
+    """
+    SuperPoker Hand #12345:  Hold'em No Limit ($2.50/$5.00 USD) - 1999-12-31 23:59:59
+    Table 'Rigel' 6-max Seat #4 is the button
+    Seat 1: Anna ($1091.61 in chips)
+    Seat 2: Bob ($581.64 in chips)
+    """
   end
 end
